949aac237a31c3f330c452453b0fb64fdaa6d06d
resty3ya <resty3ya010@gmail.com>
2023-11-28 17:27:57 +0800

    working with landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,25 @@
 const Landing = () => {
   return (
     <main>
-      <section id="landing">Landing</section>
+      <section
+        id="landing"
+        className="sm:pl-16 pl-8 bg-yellow-800 sm:pr-16 pr-8 sm:pb-24 pb-12"
+      >
+        <div className="w-full flex xl:flex-row justify-center min-h-screen">
+          <img src="" alt="" />
+          <div className="text-center">
+            <p className="text-white font-bangers text-4xl pb-6 pt-80">
+              AT THE MUSTACHE WE DO THE PASSION WITH STYLE
+            </p>
+            <button
+              type="submit"
+              className="bg-green-950 rounded-md px-5 font-nunito text-white"
+            >
+              Book now
+            </button>
+          </div>
+        </div>
+      </section>
       <section id="about">About</section>
       <section id="services">Services</section>
       <section id="booking">Booking</section>
