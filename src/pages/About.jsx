a20c402641a2944c3e5d3c9a9cb4ffbb534b3be4
resty3ya <resty3ya010@gmail.com>
2023-11-29 00:18:34 +0800

    working with about design

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,27 +2,27 @@ import { aboutImage } from "../assets/images";
 
 const About = () => {
   return (
-    <section className="justify-center min-h-screen">
+    <section className="justify-evenly flex-col-reverse min-h-screen">
       <div className="flex">
         <img src={aboutImage} alt="barber" />
+      </div>
+      <div className="">
         <h2 className="font-bangers text-white">
           we are the group of young professional barbers
         </h2>
-        <div className="font-nunito text-white">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-          <br />
-          <p>
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-            cupidatat non proident, sunt in culpa qui officia deserunt mollit
-            anim id est laborum. Lorem ipsum dolor sit amet
-          </p>
-        </div>
+        <p className="font-nunito text-white">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <br />
+        <p className="font-nunito text-white">
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum. Lorem ipsum dolor sit amet
+        </p>
       </div>
     </section>
   );
