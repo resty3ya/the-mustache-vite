d426df2f1799fe760e74d16e8a342ad8a1e18760
resty3ya <resty3ya010@gmail.com>
2023-11-28 12:10:48 +0800

    working with the landing page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import { navlinks } from "../utils/constant";
-import img from "../assets/icons/mustach-logo.svg";
+import img from "../assets/icons/the-mustache-logo-white.svg";
 
 const Navbar = () => {
   return (
-    <header className="fixed h-[78px] py-8 w-full bg-[#3B3E33]/50">
-      <nav className="flex justify-between items-center">
-        <a href="/" className="">
-          <img src={img} alt="logo" width={80} height={29} className="" />
+    <header className="fixed h-[78px] w-full bg-[#3B3E33]/50">
+      <nav className="flex justify-between">
+        <a href="/">
+          <img src={img} alt="logo" width={70} height={25} />
         </a>
-        <div className="grid-cols-4 px-10 space-x-11">
+        <div className="grid-cols-4 px-10 space-x-11 pt-10">
           {navlinks.map((item) => (
             <a
               key={item.href}
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,14 @@
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <main>
+      <section id="landing">Landing</section>
+      <section id="about">About</section>
+      <section id="services">Services</section>
+      <section id="booking">Booking</section>
+      <section id="gallery">Gallery</section>
+      <section id="prices">Prices</section>
+      <section id="testimony">Testimony</section>
+    </main>
+  );
 };
 export default Landing;
